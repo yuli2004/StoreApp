3c451d4ffccdcfe479ab4e68d10c4e4cc58a4cfe
User <User@LAB2-8>
2021-10-25 13:12:54 +0300

    updte app connection settings

--- StoreApp/StoreApp/Services/StoreAPIProxy.cs
+++ StoreApp/StoreApp/Services/StoreAPIProxy.cs
@@ -18,12 +18,12 @@ namespace StoreApp.Services
     {
         private const string CLOUD_URL = "TBD"; //API url when going on the cloud
         private const string CLOUD_PHOTOS_URL = "TBD";
-        private const string DEV_ANDROID_EMULATOR_URL = "http://10.0.2.2:21604/StoreAPI"; //API url when using emulator on android
-        private const string DEV_ANDROID_PHYSICAL_URL = "http://192.168.1.14:21604/StoreAPI"; //API url when using physucal device on android
-        private const string DEV_WINDOWS_URL = "https://localhost:44331/StoreAPI"; //API url when using windoes on development
-        private const string DEV_ANDROID_EMULATOR_PHOTOS_URL = "http://10.0.2.2:21604/Images/"; //API url when using emulator on android
-        private const string DEV_ANDROID_PHYSICAL_PHOTOS_URL = "http://192.168.1.14:21604/Images/"; //API url when using physucal device on android
-        private const string DEV_WINDOWS_PHOTOS_URL = "https://localhost:44331/Images/"; //API url when using windoes on development
+        private const string DEV_ANDROID_EMULATOR_URL = "http://10.0.2.2:42714/StoreAPI"; //API url when using emulator on android
+        private const string DEV_ANDROID_PHYSICAL_URL = "http://192.168.1.14:42714/StoreAPI"; //API url when using physucal device on android
+        private const string DEV_WINDOWS_URL = "https://localhost:44363/StoreAPI"; //API url when using windoes on development
+        private const string DEV_ANDROID_EMULATOR_PHOTOS_URL = "http://10.0.2.2:42714/Images/"; //API url when using emulator on android
+        private const string DEV_ANDROID_PHYSICAL_PHOTOS_URL = "http://192.168.1.14:42714/Images/"; //API url when using physucal device on android
+        private const string DEV_WINDOWS_PHOTOS_URL = "https://localhost:44363/Images/"; //API url when using windoes on development
 
         private HttpClient client;
         private string baseUri;
